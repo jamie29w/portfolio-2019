59a7e6cc3e24ca39959f2645e7e43a071a14ca6c
Jamie Woodmancy <jamie29w@gmail.com>
2018-09-06 14:08:49 -0700

    init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {}
+    }
+    render() {
+        return (
+            <div>hi from App</div>
+        )
+    }
+}
+
+export default App
